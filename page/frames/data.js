b5399783b8259fa7958c6ed832eaa91711dc30f1
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-05-09 13:10:18 +0300

    -

---
+++ page/frames/data.js
@@ -0,0 +1,51 @@
+// данные
+// синтаксис: форма (0=круг, 1=квадрат) | размер | колличество
+const array = [
+    [0,     90,     3],
+    [0,     128,    3],
+    [0,     168,    2],
+    [0,     208,    3],
+    [1,     290,    1],
+    [1,     295,    3],
+    [1,     395,    2]
+];
+
+let past = document.getElementById('cluster');
+
+for( i=0; i<array.length; i++ ){
+    let form = array[i][0];
+    let size = array[i][1];
+    let count = array[i][2];
+
+    if( form == 0){
+        cluster.innerHTML += `
+            <div class="card circle" style="width: ${size}px">
+                <div>
+                    <div>${size}x${size}</div>
+                    <div>mm</div>
+                </div>
+                <div>${count}</div>
+            </div>
+        `
+    } else {
+        cluster.innerHTML += `
+            <div class="card" style="width: ${size}px; height: ${size}px">
+                <div>
+                    <div>${size}x${size}</div>
+                    <div>mm</div>
+                </div>
+                <div>${count}</div>
+            </div>
+        `
+    };
+};
+
+cluster.innerHTML += `
+    <div class="card" style="width: 900px; height: 400px">
+        <div>
+            <div>400x900</div>
+            <div>mm</div>
+        </div>
+        <div>1</div>
+    </div>
+`;
